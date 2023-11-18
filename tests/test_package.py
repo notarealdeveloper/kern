aeb0c8940b588b4ab296306023878f158356cb52
Jason Wilkes <notarealdeveloper@gmail.com>
2023-11-18 17:42:38 -0600

    Add tests for pdf and image drivers

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,4 +1,15 @@
+import os
+import re
 import pytest
 
-def test_package():
-    pass
+def test_drivers_image_to_text():
+    pipe = os.popen("cat share/cats.jpg | image")
+    assert re.search('cat', pipe.read())
+
+def test_drivers_image_and_text_to_text():
+    pipe = os.popen("cat share/cats.jpg | image 'How many cats?'")
+    assert re.search('2', pipe.read())
+
+def test_drivers_pdf_to_text():
+    pipe = os.popen("cat share/chicken.pdf | pdf")
+    assert re.search('chicken', pipe.read())
