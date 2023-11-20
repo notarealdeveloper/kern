65052efa78e7d2eacaba676d1adf5dd936a33aed
Jason Wilkes <notarealdeveloper@gmail.com>
2023-11-20 12:36:22 -0800

    All tests passing after merging in multiplexed input script

--- tests/test_image.py
+++ tests/test_image.py
@@ -2,14 +2,17 @@ import os
 from drivers import image
 
 def test_image_lib_1_image_to_text():
-    path = 'root/usr/share/cats.jpg'
-    text = image.to_text(path)
+    file = 'root/usr/share/cats.jpg'
+    text = image.to_text(file)
     assert 'cat' in text
 
 def test_image_lib_1_image_and_text_to_text():
-    path = 'root/usr/share/cats.jpg'
-    text = image.and_text_to_text(path, "How many cats?")
-    assert '2' in text
+    file = 'root/usr/share/cats.jpg'
+    assert image.image_and_text_to_text(file, "What animal is this?") == 'cat'
+    assert image.image_and_text_to_text(file, "What animals are these?") == 'cats'
+    assert image.image_and_text_to_text(file, "How many are there") == '2'
+    assert image.image_and_text_to_text(file, "How many cats are there") == '2'
+    assert image.image_and_text_to_text(file, "How many dogs are there") == '0'
 
 def test_image_bin_1_image_to_text():
     pipe = os.popen("cat root/usr/share/cats.jpg | image")
