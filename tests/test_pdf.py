c5a94ee566f826b473bbdbba0f916ad82a23282b
Jason Wilkes <notarealdeveloper@gmail.com>
2023-12-04 18:56:48 -0800

    Merge suggestions from michen00 into tests/test_pdf.py

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,37 +1,15 @@
 import os
-from kern import pdf
+from kern.pdf import pdf_to_text, pdf_to_pages
 
 def test_pdf_lib():
     file = "files/chicken.pdf"
-    text = pdf.to_text(file)
+
+    text = pdf_to_text(file)
     assert 'chicken' in text
 
-    file = "files/chicken.pdf"
-    text = pdf.pdf_to_text(file)
-    assert 'chicken' in text
-
-def test_pdf_lib_return_pages():
-    file = "files/chicken.pdf"
-    text = pdf.to_text(file, return_pages=True)
-    assert not isinstance(text, str)
-    assert all(isinstance(page, str) for page in text)
-    assert 'chicken' in '\n\n'.join(text)
-
-    file = "files/chicken.pdf"
-    text = pdf.to_text(file, return_pages=False)
-    assert isinstance(text, str)
-    assert 'chicken' in text
-
-    file = "files/chicken.pdf"
-    text = pdf.pdf_to_text(file, return_pages=True)
-    assert not isinstance(text, str)
-    assert all(isinstance(page, str) for page in text)
-    assert 'chicken' in '\n\n'.join(text)
-
-    file = "files/chicken.pdf"
-    text = pdf.pdf_to_text(file, return_pages=False)
-    assert isinstance(text, str)
-    assert 'chicken' in text
+    pages = pdf_to_pages(file)
+    assert isinstance(pages, list)
+    assert all('chicken' in page for page in pages)
 
 def test_pdf_bin():
     pipe = os.popen("cat files/chicken.pdf | pdf")
