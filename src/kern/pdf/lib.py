b6568f6e62829f70730a17fa9342a9997460585c
Jason Wilkes <notarealdeveloper@gmail.com>
2023-12-04 18:55:45 -0800

    Merge suggestions from michen00 into pdf/lib.py

--- src/kern/pdf/lib.py
+++ src/kern/pdf/lib.py
@@ -1,12 +1,17 @@
 __all__ = [
+    'pdf_to_pages',
     'pdf_to_text',
 ]
 
-def pdf_to_text(file, return_pages=False) -> str | list[str]:
+def pdf_to_pages(file):
     import pypdf
     import assure
     # assure file is seekable to support stdin.
     file = assure.seekable(file)
     reader = pypdf.PdfReader(file)
     pages = [page.extract_text() for page in reader.pages]
-    return pages if return_pages else '\n\n'.join(pages)
+    return pages
+
+def pdf_to_text(file):
+    pages = pdf_to_pages(file)
+    return '\n\n'.join(pages)
