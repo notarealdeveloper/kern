4ebcfae281dff5d4b0aca51d9c13ceb11a81a7ca
Michael I Chen <michael.chen@aicadium.ai>
2023-12-05 01:54:19 +0000

    Add return_pages parameter to pdf_to_text

--- src/kern/pdf/lib.py
+++ src/kern/pdf/lib.py
@@ -2,11 +2,11 @@ __all__ = [
     'pdf_to_text',
 ]
 
-def pdf_to_text(file):
+def pdf_to_text(file, return_pages=False) -> str | list[str]:
     import pypdf
     import assure
     # assure file is seekable to support stdin.
     file = assure.seekable(file)
     reader = pypdf.PdfReader(file)
     pages = [page.extract_text() for page in reader.pages]
-    return '\n\n'.join(pages)
+    return pages if return_pages else '\n\n'.join(pages)
--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -10,6 +10,29 @@ def test_pdf_lib():
     text = pdf.pdf_to_text(file)
     assert 'chicken' in text
 
+def test_pdf_lib_return_pages():
+    file = "files/chicken.pdf"
+    text = pdf.to_text(file, return_pages=True)
+    assert not isinstance(text, str)
+    assert all(isinstance(page, str) for page in text)
+    assert 'chicken' in '\n\n'.join(text)
+
+    file = "files/chicken.pdf"
+    text = pdf.to_text(file, return_pages=False)
+    assert isinstance(text, str)
+    assert 'chicken' in text
+
+    file = "files/chicken.pdf"
+    text = pdf.pdf_to_text(file, return_pages=True)
+    assert not isinstance(text, str)
+    assert all(isinstance(page, str) for page in text)
+    assert 'chicken' in '\n\n'.join(text)
+
+    file = "files/chicken.pdf"
+    text = pdf.pdf_to_text(file, return_pages=False)
+    assert isinstance(text, str)
+    assert 'chicken' in text
+
 def test_pdf_bin():
     pipe = os.popen("cat files/chicken.pdf | pdf")
     assert 'chicken' in pipe.read()
