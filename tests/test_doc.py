155b870eafafa004cbc5b613ac1d7f154a9d3830
Jason Wilkes <notarealdeveloper@gmail.com>
2024-01-04 12:25:53 -0800

    Add support for rtf files

--- tests/test_doc.py
+++ tests/test_doc.py
@@ -10,9 +10,16 @@ def test_doc_lib():
     text = doc_to_text(file)
     assert 'baseball' in text
 
+    file = "files/football.rtf"
+    text = doc_to_text(file)
+    assert 'football' in text
+
 def test_doc_bin():
     pipe = os.popen("cat files/basketball.docx | doc")
     assert 'basketball' in pipe.read()
 
     pipe = os.popen("cat files/baseball.odt | doc")
     assert 'baseball' in pipe.read()
+
+    pipe = os.popen("cat files/football.rtf | doc")
+    assert 'football' in pipe.read()
